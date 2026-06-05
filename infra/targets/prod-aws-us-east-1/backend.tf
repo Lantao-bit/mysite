6af6186545639b228f5ae150ae46fe6743afff07
Lantao-bit <lantao1965@gmail.com>
2026-06-05 16:13:52 +0800

    Setup aws for dev-aws-us-east-1

--- infra/targets/prod-aws-us-east-1/backend.tf
+++ infra/targets/prod-aws-us-east-1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "portfolio-tfstate"
+    bucket         = "portfolio-tfstate-712416941115"
     key            = "prod-aws-us-east-1/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "portfolio-tfstate-lock"
